a22d538863db181d9fa6bb23760b880c2d10dd58
Godsway <gbaniba@st.ug.edu.gh>
2025-08-06 19:52:56 +0000

    Completed Abstract Classes and Methods

--- DCIT_318_ASSIGNMENT2/DCIT_318_ASSIGNMENT2/Program.cs
+++ DCIT_318_ASSIGNMENT2/DCIT_318_ASSIGNMENT2/Program.cs
@@ -28,13 +28,55 @@ public class Cat: Animal
     }
 }
 
+
+
+public abstract class Shape
+{
+    public abstract double GetArea();
+
+}
+
+public class Circle: Shape
+{
+    public double Radius { get; set; }
+
+    public Circle (double radius)
+    {
+        Radius = radius;
+    }
+
+    public override double GetArea()
+    {
+        return Math.PI * Radius * Radius;
+    }
+}
+
+public class Rectangle: Shape
+{
+    public double Width { get; set; }
+    public double Height { get; set; }
+
+    public Rectangle (double width, double height)
+    {
+        Width = width;
+        Height = height;
+    }
+
+    public override double GetArea()
+    {
+       return Width * Height;
+    }
+}
+
+
+
 namespace DCIT_318_ASSIGNMENT2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Inheritance and Method Overriding");
+            Console.WriteLine("\nInheritance and Method Overriding");
             Animal generic = new Animal();
             Animal dog = new Dog();
             Animal cat = new Cat();
@@ -42,6 +84,15 @@ namespace DCIT_318_ASSIGNMENT2
             generic.MakeSound();
             dog.MakeSound();
             cat.MakeSound();
+
+
+            Console.WriteLine("\nAbstract Classes and Methods ");
+            Shape circle = new Circle(5);
+            Shape rectangle = new Rectangle(4, 6);
+
+            Console.WriteLine($"Circle Area: {circle.GetArea():F2}");
+            Console.WriteLine($"Rectangle Area: {rectangle.GetArea():F2}");
+
         }
     }
 }
