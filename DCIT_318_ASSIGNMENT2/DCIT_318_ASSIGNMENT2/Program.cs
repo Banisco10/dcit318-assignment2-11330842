c3d0197398c60ddbfc289bb098e73e7a6eae07b9
Godsway <gbaniba@st.ug.edu.gh>
2025-08-06 18:56:37 +0000

    Completed Inheritance and Method Overridding

---
+++ DCIT_318_ASSIGNMENT2/DCIT_318_ASSIGNMENT2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Animal
+{
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Some generic sound");
+    }
+}
+ public class Dog: Animal
+    {
+    public override void MakeSound()
+    {
+        Console.WriteLine("Bark");
+    }
+
+}
+
+public class Cat: Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Meow");
+    }
+}
+
+namespace DCIT_318_ASSIGNMENT2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inheritance and Method Overriding");
+            Animal generic = new Animal();
+            Animal dog = new Dog();
+            Animal cat = new Cat();
+
+            generic.MakeSound();
+            dog.MakeSound();
+            cat.MakeSound();
+        }
+    }
+}
